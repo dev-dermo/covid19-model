683e2cd023f1b0f2364df268ba0a7e216b86b4c9
Diarmuid Murphy <diarmuid.com@gmail.com>
2020-03-06 00:00:53 -0800

    hard data modeled in objects

--- scripts.js
+++ scripts.js
@@ -1,1 +1,23 @@
-alert('js added');
+$(document).ready(function() {
+	// model our virus characteristics
+	var virus = {
+		name: "COVID-19",
+		reproductionRate: 0.025, 			// 2.5%
+		deathRate: 0.034, 						// 3.4%
+		infectionDuration: 2, 				// weeks
+		incubationPeriod: 2, 					// weeks
+	};
+
+	// model our environment where the virus is present
+	var environment = {
+		name: "United States of America",
+		population: 330000000, 				// 330 million
+		peopleInfected: 1,						// patient 0
+		deaths: 0,
+		survivors: 0,
+		week: 0,											// week 0
+
+	};
+
+	
+});
