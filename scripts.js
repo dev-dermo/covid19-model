2d180fc09917706cabf3cf3d6c5424dbdb30d9b4
Diarmuid Murphy <diarmuid.com@gmail.com>
2020-03-06 07:14:06 -0800

    basics working

--- scripts.js
+++ scripts.js
@@ -1,25 +1,28 @@
 $(document).ready(function() {
 	// model our virus characteristics
+
 	var virus = {
 		name: "COVID-19",
 		reproductionRate: 2.5, 				// 2.5
 		deathRate: 0.034, 						// 3.4%
-		infectionDuration: 2, 				// weeks
 		incubationPeriod: 2, 					// weeks
 		infectPeople: function() {
 			environment.newInfections*=this.reproductionRate;
 			if (environment.newInfections > environment.healthyPeople) {
 				environment.newInfections = environment.healthyPeople;
+				this.stopInterval();
 			}
 
 			environment.totalInfections+=environment.newInfections;
 			if (environment.totalInfections > environment.population) {
 				environment.totalInfections = environment.population;
+				this.stopInterval();
 			}
 
 			environment.healthyPeople-=environment.newInfections;
 			if (environment.healthyPeople < 0) {
 				environment.healthyPeople = 0;
+				this.stopInterval();
 			}
 		},
 		surviveOrDie: function() {
@@ -28,6 +31,9 @@ $(document).ready(function() {
 
 			environment.newSurvivors = environment.newInfections - environment.newDeaths;
 			environment.totalSurvivors+=environment.newSurvivors;
+		},
+		stopInterval: function() {
+			clearInterval(intervalId);
 		}
 	};
 
@@ -44,19 +50,28 @@ $(document).ready(function() {
 		totalSurvivors: 0,
 		week: 0,
 		incrementTime: function() {
-			this.week+=2;
+			this.week+=virus.incubationPeriod;
 		}
 	};
 
-	for (var i=0;environment.healthyPeople > 0;i++) {
+	console.log('On week ' + Math.floor(environment.week) + ', after ' + Math.floor(environment.totalInfections) + ' total infections, there are ' + Math.floor(environment.totalDeaths) + ' total deaths and ' + Math.floor(environment.totalSurvivors) + ' total recovered survivors.');
+
+
+	var intervalId = setInterval(function() {
 		environment.incrementTime();
 		virus.infectPeople();
 		virus.surviveOrDie();
 
-		console.log('On week ' + Math.floor(environment.week) + ', after ' + Math.floor(environment.totalInfections) + ' total infections, there are ' + Math.floor(environment.totalDeaths) + ' total deaths and ' + Math.floor(environment.totalSurvivors) + ' total recovered survivors.');
-	}
+		// console.log('On week ' + Math.floor(environment.week) + ', after ' + Math.floor(environment.totalInfections) + ' total infections, there are ' + Math.floor(environment.totalDeaths) + ' total deaths and ' + Math.floor(environment.totalSurvivors) + ' total recovered survivors.');
 
-	console.table(environment);
-	console.log('Healthy people: ' + environment.healthyPeople);
-	console.log('Sanity check: ' + (environment.totalDeaths + environment.totalSurvivors));
+		$('#total-deaths').text(Math.floor(environment.totalDeaths));
+		$('#week').text(environment.week);
+		$('#total-infections').text(Math.floor(environment.totalInfections));
+		$('#total-recovered').text(Math.floor(environment.totalSurvivors));
+		$('#healthy-people').text(Math.floor(environment.healthyPeople));
+	}, 2000);
+
+	// console.table(environment);
+	// console.log('Healthy people: ' + environment.healthyPeople);
+	// console.log('Sanity check: ' + (environment.totalDeaths + environment.totalSurvivors));
 });
